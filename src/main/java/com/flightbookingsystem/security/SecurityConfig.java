3ffb5e872e7e31b53b4398fe4ade74b88a5235a9
00tsvetelina <142497512+00tsvetelina@users.noreply.github.com>
2023-12-25 21:54:12 +0200

    changed security config to allow cors

--- src/main/java/com/flightbookingsystem/security/SecurityConfig.java
+++ src/main/java/com/flightbookingsystem/security/SecurityConfig.java
@@ -3,17 +3,30 @@ package com.flightbookingsystem.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.Arrays;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(cors -> cors
+                        .configurationSource(request -> {
+                            CorsConfiguration config = new CorsConfiguration();
+                            config.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+                            config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+                            config.setAllowedHeaders(Arrays.asList("*"));
+                            config.setAllowCredentials(true);
+                            return config;
+                        })
+                )
                 .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
-                .csrf(AbstractHttpConfigurer::disable)
-                .build();
+                .csrf(csrf -> csrf.disable());
+
+        return http.build();
     }
 }
